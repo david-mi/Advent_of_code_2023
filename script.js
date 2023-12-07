64337ce5ec59a038d3a9efdd2b2a17f6fbf47892
David Michel <david.michel2809@gmail.com>
2023-12-07 12:52:55 +0100

    feat: add day 01 part one solution

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 import { day01PartOneSolution } from "./src/day_01/part_one/solutions/part_one.js";
 import { day01PartTwoSolution } from "./src/day_01/part_two/solutions/part_two.js";
+import { day02PartOneSolution } from "./src/day_02/part_one/solutions/part_one.js";
 console.log({
     day01: {
         partOne: day01PartOneSolution,
         partTwo: day01PartTwoSolution
+    },
+    day02: {
+        partOne: day02PartOneSolution
     }
 });
---
+++ src/day_02/part_one/solutions/part_one.ts
@@ -0,0 +1,70 @@
+import { getInputPath, parseInputToArray } from "../../../helpers.js"
+
+const games = parseInputToArray(getInputPath(import.meta.url, "input"))
+
+const cubesLimit = {
+  red: 12,
+  green: 13,
+  blue: 14
+}
+
+type CubeColor = "red" | "blue" | "green"
+
+function getIdsSum(games: string[]) {
+  let idsSum = 0
+
+  gamesLoop: for (const game of games) {
+    const gameTries = game.split(";")
+    const gameId = getGameId(gameTries[0]!)
+
+    for (const gameTurn of gameTries) {
+      const cubesBag = new CubesBag()
+      const parsedCubeData = parseCubeData(extractCubeData(gameTurn))
+
+      try {
+        parsedCubeData.forEach(([amount, color]) => {
+          cubesBag.add(color, amount)
+        })
+      } catch (error) {
+        continue gamesLoop
+      }
+    }
+
+    idsSum += gameId
+  }
+
+  return idsSum
+}
+
+
+function getGameId(gameLine: string) {
+  const gameId = gameLine!.match(/\d+(?=:)/)![0]
+  return parseInt(gameId, 10)
+}
+
+class CubesBag {
+  _red = 0
+  _blue = 0
+  _green = 0
+
+  add(color: CubeColor, amount: number) {
+    this[`_${color}`] += amount
+    if (this[`_${color}`] > cubesLimit[color]) {
+      throw new RangeError("Limit reached")
+    }
+  }
+}
+
+function parseCubeData(cubesData: RegExpMatchArray): [number, CubeColor][] {
+  return cubesData.map((cubeData) => {
+    const [amount, color] = cubeData.split(/\s/) as [string, CubeColor]
+    return [parseInt(amount, 10), color]
+  })
+}
+
+function extractCubeData(game: string) {
+  const gameRegex = /(\d+) (blue|red|green)/g
+  return game.match(gameRegex)!
+}
+
+export const day02PartOneSolution = getIdsSum(games)
