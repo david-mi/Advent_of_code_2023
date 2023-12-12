f444792826735f84db47e591f6d170d64dfaf19b
David Michel <david.michel2809@gmail.com>
2023-12-12 09:37:50 +0100

    feat: init day 05 files

---
+++ src/day_05/part_one/solution.ts
@@ -0,0 +1,7 @@
+import { setInputLinesToArray } from "../../helpers.js";
+import { input } from "./inputs/input.js";
+import { inputExample } from "./inputs/input_example.js";
+
+const truc = setInputLinesToArray(inputExample)
+
+export const day05PartOneSolution = "day05PartOneSolution"
